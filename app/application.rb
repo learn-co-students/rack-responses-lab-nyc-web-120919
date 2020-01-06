2165d5b943c6cda6891820dc10c34ffa07bedc72
Alisher <Allumati@icloud.com>
2020-01-06 09:23:04 -0500

    Done.

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+        now = Time.now
+        if now.hour >= 12
+            resp.write "Good afternoon!"
+        else
+            resp.write "Good morning!"
+        end 
+        resp.finish
+    end
+end 
